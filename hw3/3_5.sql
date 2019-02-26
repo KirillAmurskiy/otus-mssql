4bb860f897b213c7e2ea503c8f9fdca7e7c5d000
Kirill Amurskiy <kirill.amurskiy@gmail.com>
2019-02-26 20:12:18 +0300

    complete hw3

--- hw3/3_5.sql
+++ hw3/3_5.sql
@@ -11,4 +11,26 @@ RECONFIGURE;
 
 SELECT @@SERVERNAME;
 
-exec master..xp_cmdshell 'bcp "[WideWorldImporters].Sales.InvoiceLines" out "C:\tmp\InvoiceLines.txt" -T -w -t, -S localhost\MSSQLSERVER';
+exec master..xp_cmdshell 'bcp "[WideWorldImporters].Sales.InvoiceLines" out "C:\tmp\InvoiceLines.txt" -T -w -t"@eu&$1&" -S localhost';
+
+select *
+into WideWorldImporters.Sales.InvoiceLines_BulkDemo
+from WideWorldImporters.Sales.InvoiceLines;
+
+
+truncate table WideWorldImporters.[Sales].[InvoiceLines_BulkDemo];
+
+BULK INSERT [WideWorldImporters].[Sales].[InvoiceLines_BulkDemo]
+				   FROM "c:\tmp\InvoiceLines.txt"
+				   WITH 
+					 (
+						BATCHSIZE = 1000, 
+						DATAFILETYPE = 'widechar',
+						FIELDTERMINATOR = '@eu&$1&',
+						ROWTERMINATOR ='\n',
+						KEEPNULLS,
+						TABLOCK        
+					  );
+
+select top(100) *
+from [WideWorldImporters].[Sales].[InvoiceLines_BulkDemo];
